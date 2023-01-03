5db5f3e34a7e1b146e69dc7ae20442f0d31dc272
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2023-01-03 13:03:49 +0300

    feat: add conditionals to popover (for elf)

--- src/components/popover-card/popover-card.component.jsx
+++ src/components/popover-card/popover-card.component.jsx
@@ -18,10 +18,16 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import { selectUserRole } from "../../store/user/userSlice";
+import { removeDeletedGift } from "../../store/elf/elfSlice";
 
-const PopoverCard = ({ gift, default_gift_url }) => {
+const GIFT_URL =
+  "https://res.cloudinary.com/doz8n2ayx/image/upload/v1671569315/christmas-wrapped-gift_bbqaql.svg";
+
+const PopoverCard = ({ gift, default_gift_url = GIFT_URL }) => {
   const dispatch = useDispatch();
   const currentPage = useSelector(selectCurrentPage);
+  const currentUserRole = useSelector(selectUserRole);
   const full_gift = JSON.parse(gift);
 
   const handleDelete = async () => {
@@ -29,6 +35,8 @@ const PopoverCard = ({ gift, default_gift_url }) => {
     await dispatch(deleteChildGiftAsync(full_gift.id));
     if (currentPage !== full_gift.total_pages)
       await dispatch(getChildGiftsAsync(currentPage));
+    if (currentUserRole === "elf")
+      dispatch(removeDeletedGift({ giftId: full_gift.id }));
   };
 
   const handleEdit = () => {
@@ -47,14 +55,26 @@ const PopoverCard = ({ gift, default_gift_url }) => {
           {full_gift.description}
         </Typography>
       </CardContent>
-      <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
-        <IconButton aria-label="delete" onClick={handleEdit}>
-          <EditIcon color="success" />
-        </IconButton>
-        <IconButton aria-label="delete" onClick={handleDelete}>
-          <DeleteIcon color="error" />
-        </IconButton>
-      </CardActions>
+      {full_gift.created_by === "child" && currentUserRole !== "elf" ? (
+        <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <IconButton aria-label="delete" onClick={handleEdit}>
+            <EditIcon color="success" />
+          </IconButton>
+          <IconButton aria-label="delete" onClick={handleDelete}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </CardActions>
+      ) : full_gift.created_by === "elf" && currentUserRole === "elf" ? (
+        <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <IconButton aria-label="delete" onClick={handleEdit}>
+            <EditIcon color="success" />
+          </IconButton>
+          <IconButton aria-label="delete" onClick={handleDelete}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </CardActions>
+      ) : null}
+
       <Modal gift={full_gift} />
     </Card>
   );
